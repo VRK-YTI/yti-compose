bf6782e1f78c19287faaa0472d0eaf6293967eae
Antti Tohmo <antti.tohmo@gofore.com>
2019-05-31 11:27:07 +0300

    YTI-394: Correcting linebreaks from CRLF -> LF and changing organization UUID to match current environments.

--- src/script/init-admin.sh
+++ src/script/init-admin.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
+#
 #----------------------------------
 docker exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user VALUES ('admin@localhost','Admin','User',true,null,null,'4ce70937-6fa4-49af-a229-b5f10328adb8')"
 #----------------------------------
 #----------------------------------
-docker exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.organization  (id,name_en,name_fi) VALUES ('25e2373c-537d-4129-ab82-c28b97d00c5f','Platform administration','Alustan yllapito')"
+docker exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.organization (id,name_en,name_fi,name_sv) VALUES ('7d3a3c00-5a6b-489b-a3ed-63bb58c26a63','Interoperability platform developers','Yhteentoimivuusalustan yllapito','Utvecklare av interoperabilitetsplattform')"
 #----------------------------------
 #----------------------------------
-docker exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user_organization VALUES ('25e2373c-537d-4129-ab82-c28b97d00c5f','ADMIN','4ce70937-6fa4-49af-a229-b5f10328adb8')"
+docker exec yti-postgres psql -U postgres -d groupmanagement -c "INSERT INTO public.user_organization VALUES ('7d3a3c00-5a6b-489b-a3ed-63bb58c26a63','ADMIN','4ce70937-6fa4-49af-a229-b5f10328adb8')"
 #----------------------------------
