36c0e1ee4c83fa2a25f3ee9fdb94ea8e7dff3c50
Kimmo Leppinen <kimmo.leppinen@compile.fi>
2020-01-30 11:51:53 +0200

    Compiles and bublisg yti-spring-security and yti-spring-migration libs

--- src/script/bootstrap.sh
+++ src/script/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+#common libraries
+commonLibs="yti-spring-security
+yti-spring-migration"
 #Component-list
-components="yti-spring-security
-yti-spring-migration
-yti-codelist-common-model
+components="yti-codelist-common-model
 yti-docker-java-base
 yti-config-server
 yti-codelist-public-api-service
@@ -33,6 +34,21 @@ clone_component_from_git () {
         `git clone $1$2.git -b$3`
 }
 
+clone_and_publish_components (){
+    cd $1
+    tags=`git describe --tags $(git rev-list --tags --max-count=2)`
+    echo "tags=$tags"
+    for tag in $tags
+    do
+	echo "Get and compile tag $tag"
+	`git checkout $tag`
+         echo "publish component:$1"
+         echo "--------------------------"
+         ./gradlew publishToMavenLocal
+         echo "DONE $1"
+    done
+    cd ..
+}
 #Main
 #Where to fetch sources
 BRANCH=master
@@ -47,10 +63,19 @@ echo Clone repositories
 mkdir -p $BUILD_BASE
 cd $BUILD_BASE
 echo "Fetching YTI into the $BUILD_BASE"
+echo "Get libraries"
+for lib in $commonLibs
+do
+    echo "Publishing $lib"
+    clone_component_from_git $YTI_REPO $lib $BRANCH
+    clone_and_publish_components $lib
+done
+echo "Get components"
 for currentComponent in $components
 do
     clone_component_from_git $YTI_REPO $currentComponent $BRANCH
 done
+
 echo TERMED
 for currentComponent in $termedComponents
 do
